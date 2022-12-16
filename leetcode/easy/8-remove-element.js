b35b78e1f20505cd43bfed7e03d68925a8e48f01
Annukul Thakran <annukulthakran@Annukuls-MacBook-Pro.local>
2022-12-16 10:41:05 +0530

    8-remove-element solved and submitted

---
+++ leetcode/easy/8-remove-element.js
@@ -0,0 +1,13 @@
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  console.log(nums);
+};
+
+removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
+
+// Very easy
